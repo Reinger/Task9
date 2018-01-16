d880acdbc1bdcf27b38097b9f54916aae00025be
Reinger <dvbolotov@edu.hse.ru>
2018-01-16 16:56:59 +0500

    Создание списка

---
+++ Task9/List.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class List
+    {
+        public int data;
+        public List next;
+
+        public List()
+        {
+            data = 0;
+            next = null;
+        }
+
+        public List(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+        public List New(int n)
+        {
+            List beg = new List();
+            for (int i = 1; i <= n; i++)
+            {
+                List p = new List(i);
+                p.next = beg;
+                beg = p;
+            }
+
+            return beg;
+        }
+
+        public void Show()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+    }
+}
--- Task9/Program.cs
+++ Task9/Program.cs
@@ -8,8 +8,32 @@ namespace Task9
 {
     class Program
     {
+        static int ReadInt(string s)
+        {
+            int k;
+            bool ok;
+
+            do
+            {
+                Console.Write("{0}", s);
+                ok = Int32.TryParse(Console.ReadLine(), out k);
+                if (!ok || k<=0)
+                {
+                    Console.WriteLine("Ошибка ввода. Нужно ввести целое число больше 0");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (!ok);
+
+            return k;
+        }
+
         static void Main(string[] args)
         {
+            int n = ReadInt("n=");
+            List l=new List();
+            l = l.New(n);
+
         }
     }
 }
